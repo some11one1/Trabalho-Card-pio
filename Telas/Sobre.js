8a453637d2d2e97ce3c866936c5dd9d1951cedbf
Diego Dutra <diegodutradoamaral616@gmail.com>
2025-11-10 15:13:25 -0300

    Add files via upload

--- Telas/Sobre.js
+++ Telas/Sobre.js
@@ -1,11 +1,16 @@
 import { View, Text, Button, TextInput } from "react-native"; //  >>>>> não esquece de importar aqui se for colocar coisas tipo TouchableOpacity
 import { usarTheme } from "../Context/ThemeContext";
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import  Nav_Menu from '../Componentes/nav_menu';
+
 export default function Sobre() {
   const { tema } = usarTheme();
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: tema.background }}>
+    <SafeAreaView style={{ flex: 1, alignItems: "center", backgroundColor: tema.background }}>
+      <Nav_Menu/>
       <Text style={{color: tema.texto}}>Sobre</Text>
 
-    </View>
+    </SafeAreaView>
   );
 }
